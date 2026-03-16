64d2180b0311d581e401d3d1557c6da98785d290
Marina Rudometova <m.rudometova@g.nsu.ru>
2026-03-16 19:07:53 +0700

    task 2-4 easy files modified

--- hw/block1/2.4/test/src/main/java/org/nsu/syspro/parprog/counters/impls/LockedCounter.java
+++ hw/block1/2.4/test/src/main/java/org/nsu/syspro/parprog/counters/impls/LockedCounter.java
@@ -1,22 +1,34 @@
 package org.nsu.syspro.parprog.counters.impls;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class LockedCounter implements Counter {
 
     private long data;
+    private final ReentrantLock lock;
 
     public LockedCounter(boolean fair) {
         this.data = 0;
-        // TODO: implement me
+        this.lock = new ReentrantLock(fair);
     }
 
     @Override
     public void increment() {
-        // TODO: implement me
+        lock.lock();
+        try {
+            data++;
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public long get() {
-        // TODO: implement me
-        return 0;
+        lock.lock();
+        try {
+            return data;
+        } finally {
+            return 0;
+        }
     }
 }
